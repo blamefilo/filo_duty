7989174f00782a2b87754cd19286d6147cbae499
filo <kloud.stu@gmail.com>
2026-06-14 02:18:31 +0800

    Normalize string quoting in fxmanifest.lua
    Convert double-quoted strings to single-quoted strings throughout fxmanifest.lua for consistent code style (fx_version, game, lua54, name, author, discord, description, version, script/file lists, dependencies). No functional changes.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,34 +1,34 @@
-fx_version "cerulean"
-game "gta5"
-lua54 "yes"
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
 
-name "filo_duty"
-author "filo studios."
-discord "https://discord.gg/bErPEKvRXg"
-description "Duty interaction points creator script by filo studios."
-version "1.0.0"
+name 'filo_duty'
+author 'filo studios.'
+discord 'https://discord.gg/bErPEKvRXg'
+description 'Duty interaction points creator script by filo studios.'
+version '1.0.0'
 
 shared_scripts {
-    "@ox_lib/init.lua",
-    "shared/sh-*.lua",
+    '@ox_lib/init.lua',
+    'shared/sh-*.lua',
 }
 
 server_scripts {
-    "server/sv-*.lua"
+    'server/sv-*.lua'
 }
 
 client_scripts {
-    "client/cl-*.lua"
+    'client/cl-*.lua'
 }
 
 escrow_ignore {
-    "data/*",
+    'data/*',
 }
 
 files {
-    "data/*",
+    'data/*',
 }
 
 dependencies {
-    "community_bridge",
+    'community_bridge',
 }
